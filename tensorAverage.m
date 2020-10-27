f9253063bb50815bedcd6f7a8535c45f9beca0bd
bistek <andrea.bistacchi@unimib.it>
2020-10-27 12:05:06 +0100

    release

--- tensorAverage.m
+++ tensorAverage.m
@@ -1,8 +1,16 @@
-function stressAverage
-% average of several stress tensors in reduced stress tensor form
-% @ 2020 by Andrea Bistacchi
-% code distributed under the GNU AGPL v3.0 license
-% created 14/07/2016, last modified 13/05/2020
+function tensorAverage
+% @ 2020 by Andrea Bistacchi, distributed under the GNU AGPL v3.0 license.
+% 
+% Created 14/07/2016, last modified 13/05/2020
+% 
+% Calculates average and confidence intervals (with bootstrapping) of several
+% stress tensors in reduced stress tensor form.
+% 
+% If you use this code, please cite this repository and Traforti, A.,
+% Massironi, M., Bistacchi, A., Zampieri, D., & Viola, G. (2018).
+% Slip-tendency analysis as a tool to constrain the mechanical properties of
+% anisotropic rocks. Journal of Structural Geology, 117, 136–147.
+% https://doi.org/10.1016/j.jsg.2018.09.001
 
 % Initialize figures etc.
 set(0,'DefaultFigureWindowStyle','docked','DefaultFigureColor','w');
